c45865fabeb2cfcffcd16ca69889d6e6fc8d86b5
yuvaraj <yuvarajnaidu00@gmail.com>
2025-03-03 13:28:31 +0800

    first commit

---
+++ src/main/java/com/yukadeeca/service_erp/ServiceErpApplication.java
@@ -0,0 +1,13 @@
+package com.yukadeeca.service_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukadeeca/service_erp/ServiceErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukadeeca.service_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
